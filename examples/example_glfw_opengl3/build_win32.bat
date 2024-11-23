90a95335489122cf71c7ccb0fe551460b8c259cd
MustachoMan <fguru974@gmail.com>
2024-11-23 10:04:59 +0400

    minor changes

--- examples/example_glfw_opengl3/build_win32.bat
+++ examples/example_glfw_opengl3/build_win32.bat
@@ -1,3 +1,4 @@
+@echo off
 @REM Build for Visual Studio compiler. Run your copy of vcvars32.bat or vcvarsall.bat to setup command-line compiler.
 @set OUT_DIR=Debug
 @set OUT_EXE=example_glfw_opengl3
@@ -6,3 +7,4 @@
 @set LIBS=/LIBPATH:..\libs\glfw\lib-vc2010-32 glfw3.lib opengl32.lib gdi32.lib shell32.lib
 mkdir %OUT_DIR%
 cl /nologo /Zi /MD /utf-8 %INCLUDES% %SOURCES% /Fe%OUT_DIR%/%OUT_EXE%.exe /Fo%OUT_DIR%/ /link %LIBS%
+pause
